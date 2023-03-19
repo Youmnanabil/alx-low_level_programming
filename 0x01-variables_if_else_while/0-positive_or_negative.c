ecaf79b488e83dc23f8bfefd5a8ba6e5e39d33a0
youmna <youmna195197@fci.bu.edu.eg>
2023-03-19 16:45:24 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
-/**
- *main - Entry point
+/*
+ * main - Entry point
  *
- *description: a c program using if else statement
- * 
- * Return: always 0 (success)
+ * Description: A c program using if statements
  *
+ * Return: 0 (success)
  */
 int main(void)
 {
-	int n ;
+	int n;
 
 	srand(time(0));
-      	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{ 
-		printf("%d is positive\n", n);
-	}
+		 printf("%d is positive\n", n);
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else
-       	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
 
