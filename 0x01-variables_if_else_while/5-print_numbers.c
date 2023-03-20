1f947ef818ae150b20009c2b6d4284566792ad9b
youmna <youmna195197@fci.bu.edu.eg>
2023-03-19 18:28:51 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: a c program prints numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int num = 0; num >= 9; num++)
+		printf("%d \n", num);
+	return (0);
+}
