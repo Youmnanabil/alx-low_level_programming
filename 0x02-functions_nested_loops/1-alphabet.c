5d167025a0958a5ad829508fcb8aa952ba83ea66
youmna <youmna195197@fci.bu.edu.eg>
2023-03-21 16:37:21 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*print_alphabets using _putchar function*/
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = "a"; x <= "z"; x++)
+		_putchar(x);
+	_putchar("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void print_alphabet(void);
 int _putchar(char c);
 
 #endif
