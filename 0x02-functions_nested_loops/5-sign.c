f025cbe6ea6b01a91d272c38fe6b481bf5729e21
youmna <youmna195197@fci.bu.edu.eg>
2023-03-21 17:16:06 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*a function that prints the sign of a number*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
