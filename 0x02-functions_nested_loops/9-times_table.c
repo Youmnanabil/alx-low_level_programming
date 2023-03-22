f259a5a059d58ebcc4f3c2b88843c7a5d31ea6f7
youmna <youmna195197@fci.bu.edu.eg>
2023-03-21 17:54:49 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 			_putchar(' ');
 			prod = num * mult;
 			if (prod <= 9)
-				_putchat(' ');
+				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
 			_putchar((prod % 10) + 48);
