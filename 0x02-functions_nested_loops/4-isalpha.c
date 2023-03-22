3f10bffbab993c9f5f8c3f151366b5626193b21d
youmna <youmna195197@fci.bu.edu.eg>
2023-03-21 17:05:26 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*a function that checks for alphabetic character*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
