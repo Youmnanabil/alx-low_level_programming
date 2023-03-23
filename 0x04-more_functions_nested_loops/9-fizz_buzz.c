21bddce040e9e6acd3bf9c8b46b7ab5c1604a4f9
youmna <youmna195197@fci.bu.edu.eg>
2023-03-23 16:29:21 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,10 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
+		if (num % 3 == 0)
 			printf("fizz");
-		else if
-			(!(num % 3 == 0) && num % 5 == 0)
-				printf("buzz");
+		else if (num % 5 == 0)
+			printf("buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
 			printf("fizzbuzz");
 		else
