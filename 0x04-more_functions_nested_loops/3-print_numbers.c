b245c701aa361e601139df3e870f9a8dd36306f9
youmna <youmna195197@fci.bu.edu.eg>
2023-03-23 08:49:25 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * Return: alway 0 success
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + 48);
+	_putchar('\n');
+}
