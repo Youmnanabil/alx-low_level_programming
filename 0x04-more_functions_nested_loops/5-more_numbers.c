4a4bcf52ca67fa87a0e0c1db805fa6adc338badb
youmna <youmna195197@fci.bu.edu.eg>
2023-03-23 09:08:15 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ * Return: always 0 success
+ */
+void more_numbers(void)
+{
+	int n, i, count;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			n = count;
+			if (cout > 9)
+			{
+				_putchar(1 + 48);
+				n = count % 10;
+			}
+			_putchar(n + 48)
+		}
+	_putchar('\n');
+}
