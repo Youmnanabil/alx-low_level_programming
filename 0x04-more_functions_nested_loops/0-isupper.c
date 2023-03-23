f38b85ff5dcef95db9db7d26470b843a05f74c60
youmna <youmna195197@fci.bu.edu.eg>
2023-03-23 08:22:52 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check the code
+ * @c: is the character
+ * Return: 1 if it is upper, 0 if not
+ */
+int _isupper(int c)
+{
+	int c = 65;
+
+	if (c <= 90)
+		return (1);
+	else
+		return (0);
+}
