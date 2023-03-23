185acecc2c488b6dc43982622d12e9bdc30055c1
youmna <youmna195197@fci.bu.edu.eg>
2023-03-23 16:18:12 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("fizz");
+		else if
+			(!(num % 3 == 0) && num % 5 == 0)
+				printf("buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("fizzbuzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
