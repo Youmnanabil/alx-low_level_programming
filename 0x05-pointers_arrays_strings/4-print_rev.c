267f0992cba2a9730800affd73cf0a0f13384047
youmna <youmna195197@fci.bu.edu.eg>
2023-03-27 16:46:15 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * followed by a new line
+ * @s: is a pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
