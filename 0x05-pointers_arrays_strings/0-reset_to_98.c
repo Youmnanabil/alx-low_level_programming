cb2c5245c0655915a2e439c9eb9710e867a04c88
youmna <youmna195197@fci.bu.edu.eg>
2023-03-27 16:05:54 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rset_to_98 - tates a pointer to int paramter
+ * and update value it points to 98
+ *
+ * Return: always 0 success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
