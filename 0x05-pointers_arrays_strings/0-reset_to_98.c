1490b5a7a3f44561dcfedaae78c9924ecf17d50c
youmna <youmna195197@fci.bu.edu.eg>
2023-03-27 16:10:36 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * rset_to_98 - tates a pointer to int paramter
- * and update value it points to 98
- *
+ * reset_to_98 - is a function that tates a pointer to
+ * int paramter and update value it points to 98
+ *@n: is the character pointer
  * Return: always 0 success
  */
 void reset_to_98(int *n)
