1f372f233e6dee847a64d65a5e2aa818ebc1bcad
youmna <youmna195197@fci.bu.edu.eg>
2023-03-31 17:12:35 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to point
+ * Return : 1 on success and -1 onerror
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+#include <stdio.h>
+
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
