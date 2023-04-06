fc7ebdc6b77908b5db08e46ee0b84837e0c1fc40
youmna <youmna195197@fci.bu.edu.eg>
2023-04-06 08:08:07 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+int acutal_prime(int n, int i);
+/**
+ * is_prime_number - says if an integer is
+ * a prime number or not
+ * @n : the number to evaluate
+ * Return: always 0 success
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n-1));
+}
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
